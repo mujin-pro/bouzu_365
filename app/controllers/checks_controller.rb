425e6a83d6f5656c82702c2c571d976ce97543bd
mujin-pro <greedy.hyena.boy@gmail.com>
2021-08-12 13:51:24 +0900

    Rubocopによるコード修正

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -1,5 +1,4 @@
 class ChecksController < ApplicationController
-
   def index
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   validates :mission, length: { maximum: 50 }
   validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ }, allow_blank: true
 
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
