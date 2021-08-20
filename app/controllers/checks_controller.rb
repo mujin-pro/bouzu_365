1c5ab5aa36cb1878cb2d33427f047a3a7873b933
mujin-pro <greedy.hyena.boy@gmail.com>
2021-08-20 18:57:35 +0900

    タイムライン機能の簡易実装

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -3,6 +3,15 @@ class ChecksController < ApplicationController
     @check = Check.last
     @check1 = Check.all.order(id: "DESC")[0]
     @check2 = Check.all.order(id: "DESC")[1]
+    @check3 = Check.all.order(id: "DESC")[2]
+    @check4 = Check.all.order(id: "DESC")[3]
+    @check5 = Check.all.order(id: "DESC")[4]
+    @check6 = Check.all.order(id: "DESC")[5]
+    @check7 = Check.all.order(id: "DESC")[6]
+    @check8 = Check.all.order(id: "DESC")[7]
+    @check9 = Check.all.order(id: "DESC")[8]
+    @check10 = Check.all.order(id: "DESC")[9]
+
     @complete = Complete.last
   end
 
